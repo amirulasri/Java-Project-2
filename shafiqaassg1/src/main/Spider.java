f5f0844382e0082fd9aba504d26307d48ef9d2bb
nurshafiqa <103927051+nurshafiqa@users.noreply.github.com>
2022-04-18 17:11:58 +0800

    Update Spider.java

--- shafiqaassg1/src/main/Spider.java
+++ shafiqaassg1/src/main/Spider.java
@@ -6,6 +6,6 @@ public class Spider extends Animal {
     
     @Override
     public void Eat (){
-        System.out.println("How and what spider eat: \n1.");
+        System.out.println("How and what spider eat: \n1.spiders eat insects and they inject their prey with poison using their fangs. The poison turns the insides of insect to a watery goop and the spider just sucks it up");
     }
 }
